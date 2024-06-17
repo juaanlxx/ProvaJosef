f30ffb75f88213f3c72e9cf8c5cbe72604b3a558
Juan Pablo <juan.sonsin@etec.sp.gov.br>
2024-06-17 17:33:43 +0000

    mudar pagina

--- botao/app/(tabs)/index.tsx
+++ botao/app/(tabs)/index.tsx
@@ -1,11 +1,12 @@
 import { Text, View, StyleSheet } from 'react-native';
+import React from 'react'
 
-import Button from '../../components/Button'
+import Home from '../../components/home'
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-
+      <Home />
     </View>
   );
 }
