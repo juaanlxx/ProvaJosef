3b12f9af713cec58e95ebbc095dbecb2ac070a82
Juan Pablo <juan.sonsin@etec.sp.gov.br>
2024-06-17 17:23:54 +0000

    save

---
+++ botao/components/sobre.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import Home from './home';
+
+export default function App(){
+    const [page, setPage] = react.useState('sobre');
+
+    const renderPage = () => {
+        if(page === 'sobre') {
+            return (
+                <View styl></View>
+            )
+        }
+    }
+}
