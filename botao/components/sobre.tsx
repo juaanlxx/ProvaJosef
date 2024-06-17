c5b3624d4b383add91f902a2ae95841c1a60d3b7
Juan Pablo <juan.sonsin@etec.sp.gov.br>
2024-06-17 17:31:18 +0000

    sadasd

--- botao/components/sobre.tsx
+++ botao/components/sobre.tsx
@@ -3,13 +3,46 @@ import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import Home from './home';
 
 export default function App(){
-    const [page, setPage] = react.useState('sobre');
+    const [page, setPage] = React.useState('sobre');
 
     const renderPage = () => {
         if(page === 'sobre') {
             return (
-                <View styl></View>
-            )
+                <View style={styles.container}>
+                    <Text style={styles.title}>Locais</Text>
+                    <TouchableOpacity style={styles.button} onPress={() => setPage('home')}>
+                        <Text style={styles.buttonText}>Clique aqui</Text>
+                    </TouchableOpacity>
+                </View>
+            );
+        } else if(page === 'home'){
+            return <Home />
         }
-    }
+    };
+
+    return <View style={styles.container}>{renderPage()}</View>
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#000',
+        marginBottom: 20,
+    },
+    button: {
+        backgroundColor: '#007BFF',
+        padding: 10,
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: '#FFFFFF',
+        fontSize: 16,
+    }
+})
