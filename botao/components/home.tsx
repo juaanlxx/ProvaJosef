23e09dbc683d2a4de1dd438dc18aa4db99fcb435
Juan Pablo <juan.sonsin@etec.sp.gov.br>
2024-06-25 22:59:01 +0000

    savee

--- botao/components/home.tsx
+++ botao/components/home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import Sobre from './sobre';
 
 export default function App(){
@@ -7,14 +7,21 @@ export default function App(){
 
     const renderPage = () => {
         if (page === 'home') {
-            return (
-                <View style={styles.container}>
-                    <Text style={styles.title}>Página Principal</Text>
-                    <Text style={styles.title}>Exemplo 1</Text>
-                    <TouchableOpacity style={styles.button} onPress={() => setPage('sobre')}>
-                        <Text style={styles.buttonText}>Explore Localizações</Text>
-                    </TouchableOpacity>
-                </View>
+                return (
+                    <View style={styles.container}>
+                      <View style={styles.imagemContainer}>
+                      <Image style={styles.usuarioImagem} source={require('../assets/images/Usuario.png')} />
+                      </View>
+                      <Text style={styles.usuario}>Usuário</Text>
+                      <Text style={styles.titulo}>GeoCheck</Text>
+                      <Text style={styles.descricao}>
+                        O "GeoCheck" é um aplicativo que permite aos usuários verificar facilmente sua localização atual por meio da obtenção das coordenadas de latitude e longitude do dispositivo móvel. Seja para navegação, planejamento de viagens ou simplesmente por curiosidade, o GeoCheck é uma ferramenta útil para explorar o mundo ao seu redor.
+                      </Text>
+                      <Image style={styles.mapaImagem} source={require('../assets/images/Mapa.png')} />
+                      <TouchableOpacity style={styles.button} onPress={() => setPage('sobre')} >
+                        <Text style={styles.buttonText}>Explorar Localizações</Text>
+                      </TouchableOpacity>
+                    </View>
             )
         } else if(page === 'sobre'){
             return <Sobre />
@@ -26,24 +33,55 @@ export default function App(){
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#fff',
+      flex: 1,
+      backgroundColor: '#33513B',
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 20,
     },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        color: '#000',
-        marginBottom: 20,
+    imagemContainer: {
+      backgroundColor: '#3b6d4d',
+      borderRadius: 50,
+      padding: 20,
+      marginBottom: 20,
+    },
+    usuarioImagem: {
+      width: 100,
+      height: 100,
+    },
+    usuario: {
+      color: '#fff',
+      fontSize: 20,
+      marginBottom: 30,
+      fontWeight: 'bold',
+    },
+    titulo: {
+      color: '#fff',
+      fontSize: 32,
+      fontWeight: 'bold',
+      marginBottom: 20,
+    },
+    descricao: {
+      color: '#fff',
+      fontSize: 16,
+      textAlign: 'justify',
+      marginBottom: 40,
+      margin: 0,
+    },
+    mapaImagem: {
+      width: 299,
+      height: 141,
+      marginBottom: 20,
     },
     button: {
-        backgroundColor: '#007BFF',
-        padding: 10,
-        borderRadius: 5,
+      backgroundColor: '#fff',
+      padding: 15,
+      borderRadius: 10,
+      marginTop: 30,
     },
     buttonText: {
-        color: '#FFFFFF',
-        fontSize: 16,
-    }
-})
+      color: '#305239',
+      fontSize: 16,
+      fontWeight: 'bold',
+    },
+  });
--- botao/components/sobre.tsx
+++ botao/components/sobre.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, Image} from 'react-native';
 import Home from './home';
 
 export default function App(){
@@ -9,11 +9,21 @@ export default function App(){
         if(page === 'sobre') {
             return (
                 <View style={styles.container}>
-                    <Text style={styles.title}>Locais</Text>
-                    <TouchableOpacity style={styles.button} onPress={() => setPage('home')}>
-                        <Text style={styles.buttonText}>Clique aqui</Text>
-                    </TouchableOpacity>
-                </View>
+                    <Text style={styles.titulo}>GeoCheck</Text>
+                    <Image style={styles.imagem} source={require('../assets/images/Aviao.png')} />
+                    <Text style={styles.subtitulo}>Localização Atual</Text>
+                    <Text style={styles.coordenadas}>Latitude: </Text>
+                    <Text style={styles.coordenadas}>Longitude: </Text>
+                    
+                <TouchableOpacity style={styles.button}>
+                  <Text style={styles.buttonText}>Obter Localização</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.buttonVoltar} onPress={() => setPage('home')}>
+                  <Text style={styles.buttonVoltarTexto}>Retornar ao Início</Text>
+                </TouchableOpacity>
+
+              </View>
             );
         } else if(page === 'home'){
             return <Home />
@@ -25,24 +35,61 @@ export default function App(){
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#fff',
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 0,
+      backgroundColor: '33513B',
     },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        color: '#000',
+    imagem:{
+        width: 301,
+        height: 105,
+        marginTop: 20,
         marginBottom: 20,
     },
+    titulo: {
+      fontSize: 32,
+      color: '#FFF',
+      marginBottom: 20,
+      fontWeight: 'bold',
+    },
+    subtitulo: {
+      fontSize: 24,
+      color: '#FFF',
+      marginBottom: 20,
+      fontWeight: 'bold',
+      marginTop: 40,
+    },
+    coordenadas: {
+      fontSize: 18,
+      color: '#FFF',
+      marginBottom: 5,
+      textAlign: 'justify',
+    },
     button: {
-        backgroundColor: '#007BFF',
-        padding: 10,
-        borderRadius: 5,
+      backgroundColor: '#FFF',
+      paddingVertical: 10,
+      paddingHorizontal: 20,
+      borderRadius: 5,
+      marginVertical: 10,
+      marginTop: 20,
     },
     buttonText: {
-        color: '#FFFFFF',
-        fontSize: 16,
-    }
-})
+      color: '#000',
+      fontSize: 16,
+      fontWeight: 'bold',
+    },
+    buttonVoltar: {
+      backgroundColor: '#556B2F',
+      paddingVertical: 10,
+      paddingHorizontal: 20,
+      borderRadius: 5,
+      marginVertical: 10,
+      marginTop: 20,
+    },
+    buttonVoltarTexto: {
+      color: '#FFF',
+      fontSize: 16,
+      fontWeight: 'bold',
+    },
+  });
